354f8c8a026a90fc9a635501130ceab9b55b3332
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-04 17:46:46 -0300

    feat: add user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UserType(models.TextChoices):
+    DEFAULT = "student"
+    COLLABORATOR = "collaborator"
+
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_type = models.CharField(
+        max_length=20,
+        default=UserType.DEFAULT,
+        choices=UserType.choices
+    )
+    is_blocked = models.BooleanField(default=False)
+
+    def __repr__(self) -> str:
+        return f"<User ({self.first_name} - {self.user_type} - {self.is_blocked})>"
