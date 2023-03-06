3b1969043d97cecfc50528ee27076d1e7581189c
Elton Luiz Scholze <elscholze@gmail.com>
2023-03-06 11:03:00 -0300

    feat: add urls in users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-# Create your views here.
+
+class UserView(ListCreateAPIView):
+    ...
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    ...
