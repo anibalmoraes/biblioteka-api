b3acc9a4eb884e8027778d5639e9654773cefcee
Winnie Brandão <winnie_brandao@yahoo.com.br>
2023-03-06 11:12:42 -0300

    feat: add BookView

--- books/views.py
+++ books/views.py
@@ -1,8 +1,16 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.pagination import PageNumberPagination
+
+from .models import Book
+from .serializers import BookSerializer
 
 
-class BookView(ListCreateAPIView):
-    ...
+class BookView(ListCreateAPIView, PageNumberPagination):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
 
 
 class BookDetailView(RetrieveUpdateDestroyAPIView):
