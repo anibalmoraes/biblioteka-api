77c334475aa7089e22cdc4e32c57384e982c3a02
Lucas <lucas-leite-menezes@hotmail.com>
2023-03-08 10:23:29 -0300

    feat: add copies view logic

--- copies/views.py
+++ copies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from .models import Copy
+from books.models import Book
+from .serializers import CopySerializer
+from rest_framework import generics
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class CopyCreateView(generics.CreateAPIView):
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+    lookup_url_kwarg = "book_id"
+
+    def perform_create(self, serializer):
+        book = get_object_or_404(Book, pk=self.kwargs.get("book_id"))
+        return serializer.save(book=book)
+
+
+class CopyView(generics.ListAPIView):
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
+
+
+class CopyDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Copy.objects.all()
+    serializer_class = CopySerializer
