b30886fa214fc82dcc7e475f269bc08abc7c78f1
Elton Luiz Scholze <elscholze@gmail.com>
2023-03-15 13:17:58 -0300

    feat: add documentation and fixed following view

--- _biblioteKA/urls.py
+++ _biblioteKA/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,10 +23,10 @@ urlpatterns = [
     path("api/", include("copies.urls")),
     path("api/", include("users.urls")),
     path("api/", include("loans.urls")),
-    # path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    # path(
-    #     "api/docs/swagger/",
-    #     SpectacularSwaggerView.as_view(url_name="schema"),
-    #     name="swagger",
-    # ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger",
+    ),
 ]
--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import BookView, BookDetailView, FollowingView, FollowersView
+from .views import BookView, BookDetailView, FollowingView
 
 urlpatterns = [
     path("books/", BookView.as_view()),
     path("books/<pk>/", BookDetailView.as_view()),
     path("books/<pk>/follow/", FollowingView.as_view()),
-    path("books/<pk>/followers/", FollowersView.as_view()),
 ]
